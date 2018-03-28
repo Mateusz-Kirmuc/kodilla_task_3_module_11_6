b240a0a7818f071d909bdc9dfb6084f06ffa8d51
Mateusz Kirmuć <mateusz.kirmuc@gmail.com>
2018-03-28 18:21:22 +0200

    Fix bug: move createColumn into Column function

--- script.js
+++ script.js
@@ -18,26 +18,21 @@ $(function() {
     this.$element = createColumn();
 
     function createColumn() {
-      // here is the code for creating the column, which you will find
-      // below
+      // Create column elements
+      var $column = $('<div>').addClass('column');
+      var $columnTitle = $('<h2>').addClass('column-title').text(self.name);
+      var $columnCardList = $('<ul>').addClass('column-card-list');
+      var $columnDelete = $('<button>').addClass('btn-delete').text('x');
+      var $columnAddCard = $('<button>').addClass('add-card').text('Add a card');
+
+      // Attach event handlers
+      $columnDelete.click(function() {
+        // self.removeColumn();
+      });
+      $columnAddCard.click(function() {
+        // self.addCard(new Card(prompt("Enter the name of the card")));
+      });
     }
   }
 
-  function createColumn() {
-    // Create column elements
-    var $column = $('<div>').addClass('column');
-    var $columnTitle = $('<h2>').addClass('column-title').text(self.name);
-    var $columnCardList = $('<ul>').addClass('column-card-list');
-    var $columnDelete = $('<button>').addClass('btn-delete').text('x');
-    var $columnAddCard = $('<button>').addClass('add-card').text('Add a card');
-
-    // Attach event handlers
-    $columnDelete.click(function() {
-      // self.removeColumn();
-    });
-    $columnAddCard.click(function() {
-      // self.addCard(new Card(prompt("Enter the name of the card")));
-    });
-
-  }
 })
