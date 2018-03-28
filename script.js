219eaa25afcf6958186fc2cbeff82189cc67a141
Mateusz Kirmuć <mateusz.kirmuc@gmail.com>
2018-03-28 17:58:53 +0200

    Create randomString function

---
+++ script.js
@@ -0,0 +1,13 @@
+$(function() {
+  /*
+   * Function creates id string consists of 10 random chars in random order
+   */
+  function randomString() {
+    var chars = '0123456789abcdefghiklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXTZ';
+    var str = '';
+    for (var i = 0; i < 10; i++) {
+      str += chars[Math.floor(Math.random() * chars.length)];
+    }
+    return str;
+  }
+})
