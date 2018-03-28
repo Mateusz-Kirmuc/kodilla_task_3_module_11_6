0cf58606ac0b17786f48efb512cbfe4c1c6eb8b8
Mateusz Kirmuć <mateusz.kirmuc@gmail.com>
2018-03-28 18:17:08 +0200

    Add section to createColumn: attach event handlers

--- script.js
+++ script.js
@@ -24,10 +24,20 @@ $(function() {
   }
 
   function createColumn() {
+    // Create column elements
     var $column = $('<div>').addClass('column');
     var $columnTitle = $('<h2>').addClass('column-title').text(self.name);
     var $columnCardList = $('<ul>').addClass('column-card-list');
     var $columnDelete = $('<button>').addClass('btn-delete').text('x');
     var $columnAddCard = $('<button>').addClass('add-card').text('Add a card');
+
+    // Attach event handlers
+    $columnDelete.click(function() {
+      // self.removeColumn();
+    });
+    $columnAddCard.click(function() {
+      // self.addCard(new Card(prompt("Enter the name of the card")));
+    });
+
   }
 })
